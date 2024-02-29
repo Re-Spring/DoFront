33146e77f3f7a271b79d35c91a4aefe5d6c6c50b
bockson <148856910+bockson@users.noreply.github.com>
2024-02-29 16:18:37 +0900

    [Feature] 메인 홈 footer 구현 및 header 검색 틀 구현

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,13 +1,29 @@
-import footercss from "../../styles/mains/footer.css";
+import  "../../styles/mains/footer.css";
 
 function Footer () {
 
     return (
         <>
-        <div className="footercss">
-            <img src="../images/footerlogo.png" alt="" className={"footerLogo"}/>
-            <div>
+        <div className="footerBox">
+            <div className="footerWrap">
+                <div className="footerLeft">
+                    <div><img src="../images/footerlogo.png" alt="" className="footerLogo"/></div>
+                    <div className="Information">
+                        <p className="footerName">(주)Re:Spring</p>
+                        <ul className="listBox">
+                            <li style={{lineHeight:"500%"}}>회사 소개 | 이용약관 | 개인정보처리방침 | 대량주문안내</li>
+                            <li>주소 : 서울특별시 서초구 서초대로77길 13</li>
+                            <li>대표자 : 박지영, 이동주, 조현진, 이선호, 신주현</li>
+                            <li>eMail : respring01@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
                 
+                <div className="iconBox">
+                    <img src="../images/instagram.png" alt="" className="footerIcon"/>
+                    <img src="../images/youtube.png" alt="" className="footerIcon"/>
+                    <img src="../images/blog.png" alt="" className="footerIcon"/>
+                </div>
             </div>
         </div>
         </>
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,19 +1,24 @@
-import headercss from "../../styles/mains/header.css";
+import  "../../styles/mains/header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-
-
 function Header() {
+
     return (
         <>
-            <div className="headerBox">
-                <a href="/"><img src="../images/logo.png" alt="" className={"logo"}/></a>
-                <div className="searchBox">
-                    <input className="searchInput" type="text" placeholder="동화 검색"/>
-                    <button className="searchBtn">
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
+            <div className="headerBox"> 
+                <div className="headerWrap">
+                    <a href="/" style={{height:"30px"}}><img src="../images/logo.png" alt="" className={"logo"}/></a>
+                    <div className="searchBox">
+                        <input className="searchInput" type="text" placeholder="동화 검색"/>
+                        <button className="searchBtn">
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                    <div className="homeLogin">
+                        <button>로그인</button>
+                        <button>회원가입</button>
+                    </div>
                 </div>
             </div>
         </>
