a9d26fcca36d1e0612914109b557eeb4765eeec5
Dongju Lee <dondjulee@gmail.com>
2024-03-20 11:45:14 +0900

    [Feature] Firebase 메시지 기능 구현 중

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './layouts/Layout';
 import Main from './pages/fairytale/Main'
@@ -16,38 +17,72 @@ import FindPw from './pages/user/FindPw';
 import SearchPage from './pages/fairytale/SearchPage'; // SearchPage 컴포넌트의 경로에 맞게 조정해주세요.
 import { SessionTimeout } from './components/auth/SessionTimeout';
 import MyBook from './pages/fairytale/MyBook';
+import { firebaseApp } from './configs/Firebase';
+import { getToken, getMessaging } from 'firebase/messaging';
+import { TokenProvider , useToken } from './components/token/TokenContext';
+
+
 
 function App() {
 
+   const { setToken } = useToken();
+
+   useEffect(() => {
+    const messaging = getMessaging(firebaseApp);
+
+    // 알림 권한 요청
+    Notification.requestPermission().then(permission => {
+      if (permission === 'granted') {
+        console.log('Notification permission granted.');
+        // FCM 토큰 획득
+        getToken(messaging, { vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY })
+          .then((currentToken) => {
+            if (currentToken) {
+              console.log("FCM Token:", currentToken);
+              localStorage.setItem('fcmToken', currentToken);
+//              setToken(currentToken); // 저장
+            } else {
+              console.log('No registration token available. Request permission to generate one.');
+            }
+          }).catch((err) => {
+            console.log('An error occurred while retrieving token. ', err);
+          });
+      } else {
+        console.log('Unable to get permission to notify.');
+      }
+    });
+  }, []);
+
   return (
+    <TokenProvider>
+        <AuthProvider>
+          <BrowserRouter>
 
-    <AuthProvider>
-      <BrowserRouter>
+            <Routes>
 
-        <Routes>
+              <Route path='/' element={<Layout/>}>
+                <Route index element={ <Main/> }/>
+                <Route path="bookContent/:fairytaleCode" element={<BookContent />} />
+                <Route path='dubing' element={ <RequireAuth><Dubing/></RequireAuth> }/>
+                <Route path="make" element={ <RequireAuth><Make/></RequireAuth> }/>
+                <Route path="info" element={ <RequireAuth><Info/></RequireAuth> }/>
+                <Route path="voice" element={ <RequireAuth><Voice/></RequireAuth> }/>
+                <Route path="search" element={<SearchPage />} />
+                <Route path="mybook" element={<MyBook />} />
+              </Route>
 
-          <Route path='/' element={<Layout/>}>
-            <Route index element={ <Main/> }/>
-            <Route path="bookContent/:fairytaleCode" element={<BookContent />} />
-            <Route path='dubing' element={ <RequireAuth><Dubing/></RequireAuth> }/>
-            <Route path="make" element={ <RequireAuth><Make/></RequireAuth> }/>
-            <Route path="info" element={ <RequireAuth><Info/></RequireAuth> }/>
-            <Route path="voice" element={ <RequireAuth><Voice/></RequireAuth> }/>
-            <Route path="search" element={<SearchPage />} />
-            <Route path="mybook" element={<MyBook />} />
-          </Route>
+              <Route path='login' element={ <PublicRoute><Login/></PublicRoute> }/>
+              <Route path='enroll' element={ <PublicRoute><Enroll/></PublicRoute> }/>
 
-          <Route path='login' element={ <PublicRoute><Login/></PublicRoute> }/>
-          <Route path='enroll' element={ <PublicRoute><Enroll/></PublicRoute> }/>
+              <Route path='findId' element={ <FindId/> }/>
+              <Route path='findPw' element={ <FindPw/> }/>
 
-          <Route path='findId' element={ <FindId/> }/>
-          <Route path='findPw' element={ <FindPw/> }/>
+            </Routes>
 
-        </Routes>
-
-      </BrowserRouter>
-      <SessionManager />
-    </AuthProvider>
+          </BrowserRouter>
+          <SessionManager />
+        </AuthProvider>
+    </TokenProvider>
 
   );
 }
---
+++ src/configs/FirebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "do-rering.firebaseapp.com",
+  projectId: "do-rering",
+  storageBucket: "do-rering.appspot.com",
+  messagingSenderId: "951803853877",
+  appId: "1:951803853877:web:cdd0ceacaff1b77096457c",
+  measurementId: "G-ZWT8F8040G"
+}
+
+export default firebaseConfig;
